a17950bd9420f8bd605b89c65559eb062eebd203
son21804a5 <sonnguyen21804@gmail.com>
2025-10-29 14:19:26 +0700

    1

---
+++ API_Console/src/main/java/com/data/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.data.repository;
+
+import com.data.model.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task getByName(String name);
+}
